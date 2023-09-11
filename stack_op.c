e96be42ac523c08022b92be5e7a24309c7da617b
Ergi004 <ergilama98@gmail.com>
2023-09-11 01:23:55 -0700

    swap part 2

--- stack_op.c
+++ stack_op.c
@@ -102,7 +102,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "l%d: can't swap, stack too short", line_number);
+		fprintf(stderr, "l%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
